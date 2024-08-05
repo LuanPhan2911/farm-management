f7c0dd8965ca98908620bbb0f030e9006d3a1931
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-08-05 21:47:45 +0700

    feat: shadcn (#1)

---
+++ components/profile-button.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+
+import { Button } from "./ui/button";
+
+export const ProfileButton = () => {
+  return (
+    <Button size={"icon"} variant={"outline"}>
+      <UserButton
+        appearance={{
+          elements: {
+            avatarBox: "w-8 h-8",
+          },
+        }}
+      />
+    </Button>
+  );
+};
