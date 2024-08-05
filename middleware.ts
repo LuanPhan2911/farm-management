b9a56f80e65e0228d1f4198fa2687d293a174a7a
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-08-05 21:42:05 +0700

    feat: localization (#1)

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware();
+import createMiddleware from "next-intl/middleware";
+import { defaultLocale, localePrefix, locales, pathnames } from "./config";
+
+const intlMiddleware = createMiddleware({
+  locales: locales,
+  defaultLocale: defaultLocale,
+  localePrefix: localePrefix,
+  pathnames: pathnames,
+});
+export default clerkMiddleware((auth, req, event) => {
+  if (isProtectedRoute(req)) auth().protect();
+  return intlMiddleware(req);
+});
 
 export const config = {
   matcher: [
@@ -8,5 +21,6 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
+    "/(vi|en)/:path*",
   ],
 };
