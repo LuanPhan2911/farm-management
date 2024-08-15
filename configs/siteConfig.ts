ececc8dc6a34007e28a66332da465f57e0558d67
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-08-15 21:44:02 +0700

    add:config(Create jobs route for frontend user #17)

---
+++ configs/siteConfig.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "NetMarket",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+};
--- navigation.ts
+++ navigation.ts
@@ -1,5 +1,5 @@
 import { createLocalizedPathnamesNavigation } from "next-intl/navigation";
-import { locales, pathnames, localePrefix } from "./config";
+import { locales, pathnames, localePrefix } from "@/configs/localeConfig";
 
 export const { Link, getPathname, redirect, usePathname, useRouter } =
   createLocalizedPathnamesNavigation({
