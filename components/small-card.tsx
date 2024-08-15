1ce8c061099394a8ebb30088f2ab113716a57f3c
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-08-15 15:00:19 +0700

    add: shadcn (Create jobs route for frontend user #17)

---
+++ components/small-card.tsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { LucideIcon } from "lucide-react";
+
+interface SmallCardProps {
+  title: string;
+  value: string;
+  icon: LucideIcon;
+  description?: string;
+}
+export const SmallCard = ({
+  icon: Icon,
+  title,
+  value,
+  description,
+}: SmallCardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-bold">{title}</CardTitle>
+        <Icon className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <div className="text-md font-semibold">{value}</div>
+        {description && (
+          <p className="text-md text-muted-foreground">{description}</p>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
